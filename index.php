8d50d090b21dffc37132cc2eb49405489779a356
David Kocher <dkocher@iterate.ch>
2003-01-03 18:23:07 +0000

    No log message.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<!doctype html public "-//w3c//dtd html 4.01 transitional//en"
-        "http://www.w3.org/tr/1999/rec-html401-19991224/loose.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	   "http://www.w3.org/TR/html4/loose.dtd">
 <?php
   include "../functions.php";
   include "../graphics.php";
