4e1f68b6b3eb40511428d1b0fc98d6207abf9f02
David Kocher <dkocher@iterate.ch>
2005-02-27 22:09:47 +0000

    *** empty log message ***

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@
     
     function echogoogle() {
             return "<tr>
-                <td colspan=\"3\" width=\"100%\" bgcolor=\"#FFFFFF\" class=\"box-dash\">
+                <td colspan=\"4\" bgcolor=\"#FFFFFF\" class=\"box-dash\">
         <script type=\"text/javascript\"><!--
 google_ad_client = \"pub-9176652804694766\";
 google_ad_width = 728;
@@ -39,14 +39,14 @@ google_color_text = \"6F6F6F\";
         <h3>FTP and SFTP Browser for Mac OS X</h3></div>
         ";
     }
-
+    
     function echosubmenu() {
     	return "
               <a id=\"index\" href=\"index.php\">about</a> | 
               <a id=\"index\" href=\"index.php#download\">download</a> | 
               <a id=\"features\" href=\"features.php\">features</a> | 
               <a id=\"changelog\" href=\"changelog.php\">changelog</a> | 
-              <a id=\"forum\" href=\"http://forums.cocoaforge.com/viewforum.php?f=9\">forum</a> | 
+              <a id=\"forum\" href=\"./forum/\">forum</a> | 
               <a id=\"source\" href=\"source.php\">source code</a> | 
               <a id=\"todo\" href=\"todo.php\">todo</a> | 
               <a id=\"license\" href=\"license.php\">license</a> |
@@ -57,28 +57,9 @@ google_color_text = \"6F6F6F\";
    		return "<div class=\"emphasized\"><a href=\"http://sudo.ch/\">sudo.ch</a> | <a href=\"http://cyberduck.ch/\">cyberduck.ch</a></div>";
     }
 
-    function echofooter($dir) {
-//        return "<tr>
-//			<td class=\"box-dash\" colspan=\"3\">
-//				<div align=\"right\">
-//        		<img src=\"img/made_with_cocoa.png\" width=\"150\" height=\"60\" alt=\"Made with Cocoa\" border=\"0\"></img>
-//        		</div>
-//      	</td>
-//        </tr>";
-//		return "<a  href=\"http://sourceforge.net\"><img  src=\"http://sourceforge.net/sflogo.php?group_id=106337&amp;type=4\" width=\"125\" height=\"37\" border=\"0\" alt=\"SourceForge.net Logo\"  /></a>";
-    }
-    
-    function echoDonation() {
-        return "<tr>
-                  <td colspan=\"3\" class=\"alert\">
-                    Support the development of Cyberduck by making a donation :)
-			     </td>
-                </tr>";
-    }
-    
     function echoSoftwarePatents() {
 /*        return "<tr>
-                  <td colspan=\"3\" class=\"sp\">
+                  <td colspan=\"4\" class=\"sp\">
 		<div align=\"right\">
 			<a href=\"http://petition.eurolinux.org\"><img src=\"http://aful.org/images/patent_button.png\" alt=\"Help us to Fight For a Software Patent Free Europe\" border=\"0\"></img></a>
    		</div>
--- index.php
+++ index.php
@@ -11,11 +11,9 @@
 				<?php echo(echogoogle());?>
 				<tr>
 					<td width="200" colspan="2" valign="middle" class="box-solid">
-						<div align="center">
-							<img src="img/cyberduck.icon.jpg" width="128" height="128" alt="Cyberduck" border="0" />
-						</div>
+						<div align="center"><img src="img/cyberduck.icon.jpg" width="128" height="128" alt="Cyberduck" border="0"></img></div>
 					</td>
-					<td width="100%" class="box-solid">
+					<td colspan="2" class="box-solid">
 						<div class="underline">
                         <?php echo(echomenu(".."));?>
 						</div><?php echo(echotitle());?>
@@ -25,28 +23,55 @@
 					</td>
 				</tr>
 				<tr>
-					<td width="200" colspan="2" valign="middle" class="box-solid">
-						<p><em>"There are a few free FTP clients for the Mac, but none is as polished or as easy to use as Cyberduck."
-							</em>(<a href="http://www.macworld.com/2004/10/features/bargainsweb/index.php">macworld.com</a>)
+					<td width="200" colspan="2" valign="top" class="box-solid">
+						<p><em>"There are a few free FTP clients for the Mac, but none is as polished or as easy to use as Cyberduck." </em>(<a href="http://www.macworld.com/2004/10/features/bargainsweb/index.php">macworld.com</a>)
 						</p>
 						<div align="center">
 							<a href="http://www.macworld.com/subject"><img src="./img/MW_4mouselogo_blu.png" width="90" height="67" alt="Macworld Rating" border="0" /></a>
 						</div>
 						<p>
-							<em>All the other FTP clients I've used have been hard to learn and are confusing, but I've never had to look at the help file for Cyberduck.</em>(<a href="http://www.boingboing.net/2005/01/07/cyberduck_ftp_browse.html">boingboing.net</a>)
+							<em>"All the other FTP clients I've used have been hard to learn and are confusing, but I've never had to look at the help file for Cyberduck."</em> (<a href="http://www.boingboing.net/2005/01/07/cyberduck_ftp_browse.html">boingboing.net</a>)
 						</p>
 					</td>
-					<td width="100%" bgcolor="#FFFFFF" valign="top" class="box-dash">
+					<td bgcolor="#FFFFFF" valign="top" class="box-dash">
 						<p>Cyberduck is an <a href="http://opensource.org/docs/definition.php">open source</a> SFTP (SSH Secure File Transfer) and FTP browser licenced under the <a href="http://www.gnu.org/licenses/gpl.html">GPL</a>. It has been built from the ground up with usability in mind, having the same consistent graphical user interface for both SFTP and FTP browsing. Multiple connections are supported. Drag and drop is supported consequently for transferring files between server and client. A transfer queue keeps track of the pending file transfers and supports resuming of both downloads and uploads. Local files can be synchronized with files on the server. System technologies such as the <a href="http://www.apple.com/macosx/features/security/">Keychain</a>, <a href="http://www.apple.com/macosx/features/rendezvous/">Rendezvous</a> and <a href="http://apple.com/applescript/">AppleScript</a> are supported. Cyberduck integrates seamlessly with external editors such as <a href="http://www.codingmonkeys.de/subethaedit/">SubEthaEdit</a>, <a href="http://barebones.com">BBEdit</a>, <a href="http://macromates.com/">TextMate</a> and others. Cyberduck has been translated into numerous languages including <em>English, Dutch, German, French, Italian, Japanese, Korean, Chinese (Traditional and Simplified), Portuguese, Spanish, Finnish, Slovak, Czech, Norwegian and Russian</em>.
 						</p>
 						<p>This software is written by <a href="mailto:dkocher@cyberduck.ch">David V. Kocher</a>, Switzerland. Please have a look at the credits to see who contributed.<br />
 							I appreciate any comments, feature suggestions and bug reports.
 						</p>
+						<p class="emphasized"><strong>This program is <a href="http://www.gnu.org/philosophy/free-sw.html">free</a>. Free software is a matter of the users' freedom to run, copy, distribute, study, change and improve the software. If you find this program useful, please consider making a <a href="./donate">donation</a>. A donation would not only demonstrate your appreciation of this software, but also help to advance development in the future. The easiest way to donate is to through PayPal - it's fast, free and secure. PayPal accepts all major credit cards and electronic checks.</strong></p>
+						<div align="center">
+							<a href="./donate"><img src="https://www.paypal.com/en_US/i/btn/x-click-but04.gif" alt="Make payments with PayPal - it's fast, free and secure!"></img></a>
+						</div>
+					</td>
+					<td width="200" bgcolor="#FFFFFF" valign="top" class="box-dash">
+						<div class="underline">
+						<p><strong>Mailing List.</strong> You can subscribe to the <a href="http://lists.cyberduck.ch/mailman/listinfo/cyberduck-news">Cyberduck-news</a> mailing list to get a notification when a new version is released.
+						</p>
+						<form method="post" action="http://lists.cyberduck.ch/mailman/subscribe/cyberduck-news">
+							<p>Your email address:
+								<input type="text" name="email" value="" />
+								<input type="submit" name="email-button" value="Subscribe" />
+							</p>
+						</form>
+						</div>
+						<div class="underline">
+						<p><strong>Forum.</strong> Discuss anything related to Cyberduck in the forum hosted at <a href="./forum">cocoaforge.com</a></p>
+						<div align="center"><a href="./forum"><img width="150" border="0" alt="cocoaforge.com" src="./img/cocoaforge.gif"></img></a></div>
+						</div>
+						<p><strong>T-Shirts and Mugs.</strong></p>
+						<div align="center" id="image">
+							<a href="http://www.cafeshops.com/cyberduck">
+								<img src="img/store.jpg" width="128" height="128" align="middle" alt="Shop Cyberduck Gadgets" border="0" />
+							</a>
+						</div>
+						<p>Go shopping for Cyberduck gadgets in the <a href="http://www.cafeshops.com/cyberduck">Cafepress Cyberduck shop</a>!
+						</p>
 					</td>
 				</tr>
 				<!-- ======= Screenshot ======== -->
 				<tr>
-					<td colspan="3">
+					<td colspan="4">
 						<div align="center" id="image">
 							<a href="./img/browser.jpg">
 								<img align="middle" width="700" height="454" border="0" src="./img/browser.thumb.jpg" alt="Cyberduck Screenshot" />
@@ -54,48 +79,16 @@
 						</div>
 					</td>
 				</tr>
-				<!-- ======= Donate! ======== -->
-		            <?php echo(echoDonation());?>
-				<tr>
-					<td width="200" colspan="2" valign="middle" class="box-solid">
-						<div align="center">
-							<p>Make a donation!</p>
-							<a href="./donate"><img src="https://www.paypal.com/en_US/i/btn/x-click-but04.gif" alt="Make payments with PayPal - it's fast, free and secure!"></img></a>
-						</div>
-					</td>
-					<td width="100%" bgcolor="#FF9900" valign="top" class="box-dash">
-						<p class="emphasized"><strong>This program is <a href="http://www.gnu.org/philosophy/free-sw.html">free</a>. Free software is a matter of the users' freedom to run, copy, distribute, study, change and improve the software. If you find this program useful, please consider making a donation. A donation would not only demonstrate your appreciation of this software, but also help to advance development in the future. The easiest way to donate is to through PayPal - it's fast, free and secure. PayPal accepts all major credit cards and electronic checks.
-							</strong>
-						</p>
-					</td>
-				</tr>
-				<tr>
-					<td width="200" colspan="2" valign="middle" class="box-solid">
-						<div align="center" id="image">
-							<a href="http://www.cafeshops.com/cyberduck">
-								<img src="img/store.jpg" width="128" height="128" align="middle" alt="Shop Cyberduck Gadgets" border="0" />
-							</a>
-						</div>
-					</td>
-					<td width="100%" bgcolor="#FFFFFF" valign="top" class="box-dash">
-						<p>Go shopping for Cyberduck gadgets in the <a href="http://www.cafeshops.com/cyberduck">Cafepress Cyberduck shop</a>!
-						</p>
-					</td>
-				</tr>
 				<!-- ======= Download! ======== -->
 				<tr>
-					<td width="200" colspan="2"></td>
-					<td width="100%" class="box-solid"><strong>download</strong></td>
+					<td colspan="4" class="box-solid"><strong>download</strong></td>
 				</tr>
-				<!-- ======= 10.3 Alert ======== -->
+				<!-- ======= Donate! ======== -->
 				<tr>
-					<td colspan="3" class="alert">If Cyberduck crashes when launching, make sure you are running Mac OS X 10.3 and try installing the latest Java udpate from <a href="http://www.apple.com/support/downloads/javaupdate142.html">support.apple.com</a>.
+					<td colspan="4" class="alert">
+						Support the development of Cyberduck by making a <a href="./donate">donation</a> :)
 					</td>
-				</tr>
-				<tr>
-					<td colspan="3" class="alert"><strong>Requirements</strong>: As of version 2.4, Cyberduck is not compatible with any OS X version prior to 10.3. <a href="./changelog.php">Previous versions</a> may run on 10.2 with some limitations; your mileage may vary.
-					</td>
-				</tr>
+                </tr>
 				<tr>
 					<td width="200" colspan="2" valign="middle" class="box-solid">
 						<div align="center">
@@ -105,42 +98,25 @@
 					</td>
 					<td bgcolor="#FFFFFF" valign="top" class="box-dash">
 						<a name="download"</a>
+						<!-- ======= 10.3 Alert ======== -->
+						<p class="emphasized"><strong>Java Update.</strong> If Cyberduck crashes when launching, make sure you are running Mac OS X 10.3 and try installing the latest Java udpate from <a href="http://www.apple.com/support/downloads/javaupdate142.html">support.apple.com</a>.</p>
+						<p class="emphasized"><strong>Requirements.</strong> As of version 2.4, Cyberduck is not compatible with any OS X version prior to 10.3. <a href="./changelog.php">Previous versions</a> may run on 10.2 with some limitations; your mileage may vary.</p>
 						<p>Please have a look at the <a href="./changelog.php">changelog</a>.</p>
 						<ul class="emphasized">
 							<li><strong>2.4b4</strong> <em>February-03-2005</em><br />
 								<a href="./Cyberduck-2.4b4.dmg">Cyberduck-2.4b4.dmg</a> <a href="./Cyberduck-2.4b4.dmg"><i>(Download)</i></a>
 							</li>
 						</ul>
+					</td>
+					<td width="200" bgcolor="#FFFFFF" valign="top" class="box-dash">
 						<p>
 							You may also want to install <a href="http://growl.info">Growl</a>, a global notification system Cyberduck supports:
 						</p>
-						<a href="http://growl.info"><img src="./img/getgrowl.png" alt="getgrowl" border="0" /></a>
-					</td>
-				</tr>
-				<!-- ======= Mailing list ======== -->
-				<tr>
-					<td width="200" colspan="2"></td>
-					<td class="box-solid"><strong>mailing list</strong></td>
-				</tr>
-				<tr>
-					<td width="200" colspan="2" valign="middle" class="box-solid">
-						<div align="center">
-							<img src="img/mailman.jpg" alt="Mailing list" border="0" />
-						</div>
-					</td>
-					<td width="100%" bgcolor="#FFFFFF" valign="top" class="box-dash">
-						<p>You can subscribe to the <a href="http://lists.cyberduck.ch/mailman/listinfo/cyberduck-news">Cyberduck-news</a> mailing list to get a notification when a new version is released.
-						</p>
-						<form method="post" action="http://lists.cyberduck.ch/mailman/subscribe/cyberduck-news">
-							<p>Your email address:
-								<input type="text" name="email" size="30" value="" />
-								<input type="submit" name="email-button" value="Subscribe" />
-							</p>
-						</form>
+						<div align="center"><a href="http://growl.info"><img src="./img/getgrowl.png" alt="getgrowl" border="0" /></a></div>
 					</td>
 				</tr>
 				<tr>
-					<td colspan="3" width="100%">
+					<td colspan="4" width="100%">
 						<div align="right">
 							<a href="http://validator.w3.org/check/referer">Validate</a>, $Date$
 						</div>
