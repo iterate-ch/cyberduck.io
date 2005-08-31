90ff1725f7673dbc59b140957d1f79c54e4e5c88
David Kocher <dkocher@iterate.ch>
2005-08-31 22:10:05 +0000

    *** empty log message ***

--- donate/index.php
+++ donate/index.php
@@ -9,22 +9,19 @@
   <meta name="generator" content=
   "HTML Tidy for Mac OS X (vers 12 April 2005), see www.w3.org" />
   <?php echo(echoheader("~dkocher | cyberduck | donate", "..")); ?>
-<style type="text/css">
-/*<![CDATA[*/
- div.c4 {text-align: right}
- input.c3 {width:62px; height:31px}
- div.c2 {text-align: left}
- div.c1 {text-align: center}
-/*]]>*/
-</style>
+  <style type="text/css">
+  /*<![CDATA[*/
+   input.c3 {width:62px; height:31px}
+  /*]]>*/
+  </style>
 </head>
 
 <body id="donate">
-  <div align="center" class="standard">
+ <div align="center" class="standard">
     <table border="0" cellpadding="5" cellspacing="5" width="700">
       <tr>
         <td class="box-solid" width="150" valign="middle">
-          <div class="c1">
+          <div align="center">
             <img src="../img/cyberduck.icon.png" width="128"
             height="128" alt="Cyberduck" border="0" />
           </div>
@@ -36,7 +33,7 @@
           </div><br />
           <?php echo(echotitle());?>
 
-          <div class="c2">
+          <div align="left">
             <?php echo(echosubmenu(".."));?>
           </div>
         </td>
@@ -49,13 +46,18 @@
 
       <tr>
         <td width="150" valign="middle" class="box-solid">
-          <div class="c1">
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="image" class="c3" src="https://www.paypal.com/en_US/i/btn/x-click-but04.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
-<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHsQYJKoZIhvcNAQcEoIIHojCCB54CAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYA9vSN28Jo3ZoVNYV5h8+CB3F0DXr7upmkNPZcSVThT6nsIBRYJHokU3wZ8hzwjTn3hCj/9Un4vrkGPyc1eL0yyElyzC0Uli6EBGU8fQvCAD4z4L5u2sYWtwLWb1o8nWOgBTBxJTlNi/vW4CDx8cJJZV5zmusiLJq2s6ySVkUVLwzELMAkGBSsOAwIaBQAwggEtBgkqhkiG9w0BBwEwFAYIKoZIhvcNAwcECIdE76r06r88gIIBCDASlielK1v00/oo2QS4M9xT5T4jx/UdUutot6gbJ22D9REL4BEVcBWr/9GhUGQ0TdVfJ2NGev/HGnaxdcB44AklLL9yr/UV4/9zX9Psz3xFSxsxv/SLTA37t2vXJQ49+2stCHXuPMABzJvAQaCIAiRX3alHGSkG/0xgmSSM3VvCqyf3vvMsalhs6Bv7JOdIda7hHmdvUTwLsg9eHk7IcP8Le+gMN3/KY+DqQLcJ2UP5Z5Z9WGyLo98xK+vwDNNDILiGAyLyug30uQflb45WD0Q4I7y4pnF2ODQDwaxwN+KsZD4xHpxYS5RedppYoWmkHa9CDLWoN0qh+YInN1q6u4Righ7/89becKCCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTA1MDgzMTIxNDc1OFowIwYJKoZIhvcNAQkEMRYEFJjKydrFzmZnFnxIXp9NBkY3HX/8MA0GCSqGSIb3DQEBAQUABIGACz+n/etSni+iWtzmgz1J0kZVsozO2DXJ4eGmSlGr57BGyFYdLH7xlaKiVsyT3uQZi5GiHXfgsRJOw7YgYnvNeYxhAHBZUv8qPHotwjv4DFpWZLRGJBM1Jx5fUNn0fmjS1my4u2Zn6oICxoQDmppTEwOsfV29oUj4I3lZbJksgSY=-----END PKCS7-----
-">
-</form>          </div>
+          <div align="center">
+            <form action="https://www.paypal.com/cgi-bin/webscr"
+            method="post">
+              <input type="hidden" name="cmd" value=
+              "_s-xclick" /><input type="image" class="c3" src=
+              "https://www.paypal.com/en_US/i/btn/x-click-but04.gif"
+              border="0" name="submit" alt=
+              "Make payments with PayPal - it's fast, free and secure!" /><input type="hidden"
+              name="encrypted" value=
+              "-----BEGIN PKCS7-----MIIHsQYJKoZIhvcNAQcEoIIHojCCB54CAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYA9vSN28Jo3ZoVNYV5h8+CB3F0DXr7upmkNPZcSVThT6nsIBRYJHokU3wZ8hzwjTn3hCj/9Un4vrkGPyc1eL0yyElyzC0Uli6EBGU8fQvCAD4z4L5u2sYWtwLWb1o8nWOgBTBxJTlNi/vW4CDx8cJJZV5zmusiLJq2s6ySVkUVLwzELMAkGBSsOAwIaBQAwggEtBgkqhkiG9w0BBwEwFAYIKoZIhvcNAwcECIdE76r06r88gIIBCDASlielK1v00/oo2QS4M9xT5T4jx/UdUutot6gbJ22D9REL4BEVcBWr/9GhUGQ0TdVfJ2NGev/HGnaxdcB44AklLL9yr/UV4/9zX9Psz3xFSxsxv/SLTA37t2vXJQ49+2stCHXuPMABzJvAQaCIAiRX3alHGSkG/0xgmSSM3VvCqyf3vvMsalhs6Bv7JOdIda7hHmdvUTwLsg9eHk7IcP8Le+gMN3/KY+DqQLcJ2UP5Z5Z9WGyLo98xK+vwDNNDILiGAyLyug30uQflb45WD0Q4I7y4pnF2ODQDwaxwN+KsZD4xHpxYS5RedppYoWmkHa9CDLWoN0qh+YInN1q6u4Righ7/89becKCCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTA1MDgzMTIxNDc1OFowIwYJKoZIhvcNAQkEMRYEFJjKydrFzmZnFnxIXp9NBkY3HX/8MA0GCSqGSIb3DQEBAQUABIGACz+n/etSni+iWtzmgz1J0kZVsozO2DXJ4eGmSlGr57BGyFYdLH7xlaKiVsyT3uQZi5GiHXfgsRJOw7YgYnvNeYxhAHBZUv8qPHotwjv4DFpWZLRGJBM1Jx5fUNn0fmjS1my4u2Zn6oICxoQDmppTEwOsfV29oUj4I3lZbJksgSY=-----END PKCS7----- " />
+            </form>
+          </div>
         </td>
 
         <td colspan="2" valign="top" class="box-dash">Donate with a
@@ -82,7 +84,7 @@
 
       <tr>
         <td colspan="3">
-          <div class="c4">
+          <div align="right">
             <a href=
             "http://validator.w3.org/check/referer">Validate</a>,
             $Date$
