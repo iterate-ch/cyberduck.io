658740d1b70b77a17e252f20bf843fde6c7f8076
David Kocher <dkocher@iterate.ch>
2005-11-21 14:01:38 +0000

    *** empty log message ***

--- features.php
+++ features.php
@@ -19,335 +19,136 @@
   </div>
 
   <div id="content">
-    <table border="0" cellpadding="5" cellspacing="5" width="100%">
-      <tr>
-        <td class="box-solid" valign="top"><strong>Browse SFTP and
-        FTP servers</strong></td>
-
-        <td colspan="2" valign="top" class="box-dash">
-          <ul>
-            <li>User friendly interface!</li>
-
-            <li>Open multiple simultaneous connections</li>
-
-            <li>Document based interface</li>
-
-            <li>Support for various encodings</li>
-
-            <li>Live filtering of directory listings</li>
-
-            <li>Caching folder listings</li>
-
-            <li>Move files</li>
-
-            <li>Cut &amp; Paste files</li>
-
-            <li>Create new files</li>
-
-            <li>Rename files</li>
-
-            <li>Delete files</li>
-            <!--<li>SOCKS Proxy support</li>-->
-
-            <li>Show hidden files</li>
-
-            <li>Allow the selection of files in the browser by
-            typing the filename</li>
-          </ul>
-        </td>
-      </tr><?php echo(echogoogle());?>
-
-      <tr>
-        <td class="box-solid" valign="top">
-        <strong>Localizations</strong></td>
-
-        <td valign="top" colspan="2" class="box-dash"><em>English,
-        Czech, Dutch, Finnish, French, German, Italian, Japanese,
-        Korean, Norwegian, Portuguese, Slovak, Spanish, Chinese
-        (Traditional and Simplified), Russian, Swedish, Hungarian,
-        Danish, Polish, Indonesian and Catalan.</em> Many thanks to
-        all <a href=
-        "http://cvs.cyberduck.ch/*checkout*/cyberduck-cocoa/Credits.html?rev=HEAD&amp;content-type=text/html">
-        contributors</a>!</td>
-      </tr>
-
-      <tr>
-        <td class="box-solid" valign="top"><strong>Seamless
-        integration with external editors</strong><br /></td>
-
-        <td valign="top" colspan="2" class="box-dash">
-          <a href=
-          "http://www.codingmonkeys.de/subethaedit/">SubEthaEdit</a>,
-          <a href=
-          "http://www.barebones.com/products/bbedit/">BBEdit</a>,
-          <a href=
-          "http://www.barebones.com/products/textwrangler">TextWrangler</a>,
-          <a href="http://www.tex-edit.com/">Text-Edit Plus</a>,
-          <a href="http://macromates.com/">TextMate</a>, <a href=
-          "http://mimikaki.net/">mi</a>, <a href=
-          "http://smultron.sourceforge.net/">Smultron</a>, <a href=
-          "http://www.artman21.net/product/JeditX/">JeditX</a>,
-          <a href="http://macrabbit.com/cssedit/">CSSEdit</a>,
-          <a href="http://www.aynimac.com/">CotEditor</a>, <a href=
-          "http://www.talacia.com/">Tag</a>
-
-          <div align="center">
-            <img src="./img/subethaedit.png" height="64" alt=
-            "Subethaedit" border="0" /> <img src=
-            "./img/textmate.png" height="64" alt="TextMate" border=
-            "0" /> <img src="./img/bbedit.png" height="64" alt=
-            "BBEdit" border="0" /> <img src=
-            "./img/textwrangler.png" height="64" alt="Textwrangler"
-            border="0" /> <img src="./img/cssedit.png" height="64"
-            alt="CSSEdit" border="0" />
-          </div>
-        </td>
-      </tr>
-
-      <tr>
-        <td class="box-solid" valign="top">
-        <strong>Bookmarking</strong></td>
-
-        <td valign="top" colspan="2" class="box-dash">
-          <ul>
-            <li>Drag and drop to save bookmark as a file</li>
-
-            <li>Drag proxy icon in the window title bar to the
-            Finder</li>
-
-            <li>Drag proxy icon in the window title bar to the
-            bookmark drawer</li>
-
-            <li>Drop files onto bookmarks to upload to remote
-            host</li>
-
-            <li>Synchronize bookmarks with <a href=
-            "http://www.apple.com/macosx/features/dotmacsync/">.Mac
-            iDisk</a></li>
-
-            <li><a href=
-            "http://www.apple.com/macosx/features/spotlight/">Spotlight</a>
-            Importer for bookmark files</li>
-          </ul>
-
-          <div align="center">
-            <img src="./img/spotlight.jpg" height="64" alt=
-            "Spotlight" border="0" /> <img src="./img/document.jpg"
-            height="64" alt="Document" border="0" /> <img src=
-            "./img/idisk.jpg" height="64" alt="iDisk" border="0" />
-          </div>
-        </td>
-      </tr>
-
-      <tr>
-        <td class="box-solid" valign="top"><strong>Supporting all
-        major transfer protocols</strong></td>
-
-        <td colspan="2" valign="top" class="box-dash">
-          <ul>
-            <li>FTP (File Transfer Protocol</li>
-
-            <li>FTP/TLS (FTP secured over SSL/TLS)</li>
-
-            <li>SFTP (SSH Secure File Transfer</li>
-          </ul>
-        </td>
-      </tr>
-
-      <tr>
-        <td class="box-solid" valign="top"><strong>Keychain
-        support</strong></td>
-
-        <td valign="top" colspan="2" class="box-dash">
-          <ul>
-            <li>All passwords are stored in the system <a href=
-            "http://www.apple.com/macosx/features/security/">Keychain</a>
-            as Internet passwords available also to third party
-            applications</li>
-          </ul>
-        </td>
-      </tr>
-
-      <tr>
-        <td class="box-solid" valign="top"><strong>Secure file
-        transfers with SFTP</strong></td>
-
-        <td valign="top" colspan="2" class="box-dash">
-          <ul>
-            <li>Public key authentication</li>
-
-            <li>Keyboard Interactive (PAM) Authentication</li>
-
-            <li>Support for various encryption ciphers (3DES,
-            Blowfish, Twofish, AES, CAST)</li>
-
-            <li>Authentication algorithms (md5, sha1)</li>
-          </ul>
-        </td>
-      </tr>
-
-      <tr>
-        <td class="box-solid" valign="top">
-        <strong>Transfers</strong></td>
-
-        <td valign="top" colspan="2" class="box-dash">
-          <ul>
-            <li>Resume downloads (FTP and SFTP)</li>
-
-            <li>Resume uploads (FTP and SFTP)</li>
-
-            <li>Download folders (also <em>recursive</em>)</li>
-
-            <li>Upload folders (also <em>recursive</em>)</li>
-
-            <li>Drag and drop to transfer files (Finder &harr;
-            Cyberduck)</li>
-          </ul>
-        </td>
-      </tr>
-
-      <tr>
-        <td class="box-solid" valign="top">
-        <strong>Synchronize</strong></td>
-
-        <td valign="top" colspan="2" class="box-dash">
-          <ul>
-            <li>Synchronize local with remote directories (and vice
-            versa)</li>
-
-            <li>Choose to download/upload missing files</li>
-
-            <li>Get a preview of affected files before any action
-            is taken</li>
-          </ul>
-        </td>
-      </tr>
-
-      <tr>
-        <td class="box-solid" valign="top"><strong>Modify
-        permissions</strong></td>
-
-        <td colspan="2" valign="top" class="box-dash">
-          <ul>
-            <li>Modify permissions on multiple files</li>
-
-            <li>Modify permissions recursivly</li>
-          </ul>
-        </td>
-      </tr>
-
-      <tr>
-        <td class="box-solid" valign="top"><strong>Bonjour
-        support</strong></td>
-
-        <td valign="middle">
-          <div align="center">
-            <img src="./img/rendezvous.png" alt="Bonjour support"
-            border="0" />
-          </div>
-        </td>
-
-        <td valign="top" class="box-dash">
-          <ul>
-            <li>Auto discovery of services on the local
-            network</li>
-          </ul>
-        </td>
-      </tr>
-
-      <tr>
-        <td class="box-solid" valign="top"><strong>AppleScript
-        support</strong></td>
-
-        <td valign="middle">
-        <div align="center">
-            <img src="./img/applescript.jpg" height="64" alt=
-            "Applescript" border="0" />
-          </div>
-        </td>
-
-        <td valign="top" class="box-dash">
-          <ul>
-            <li>Full AppleScript integration. See the <a href=
-            "http://cvs.cyberduck.ch/cyberduck-cocoa/AppleScript%20Samples/">
-            sample scripts</a> (included on the disk image).</li>
-          </ul>
-        </td>
-      </tr>
-
-      <tr>
-        <td class="box-solid" valign="top"><strong>Growl
-        support</strong></td>
-
-        <td valign="middle">
-        <div align="center">
-            <img src="./img/growl.png" height="64" alt="Growl"
-            border="0" />
-          </div>
-        </td>
-
-        <td valign="top" class="box-dash">
-          <ul>
-            <li>Support for growl, the global notification system
-            for Mac OS X. See <a href=
-            "http://growl.info/">growl.info</a> for more
-            information.</li>
-          </ul>
-        </td>
-      </tr>
-
-      <tr>
-        <td class="box-solid" valign="top"><strong>Finder
-        integration</strong></td>
-
-        <td valign="middle">
-        <div align="center">
-            <img src="./img/document.jpg" height="64" alt="Growl"
-            border="0" />
-          </div>
-        </td>
-
-        <td valign="top" class="box-dash">
-          <ul>
-            <li>Use as default system wide protocol handler;
-            requires <a href=
-            "http://www.rubicode.com/Software/RCDefaultApp">RCDefaultApp</a>.</li>
-
-            <li>Opens .inetloc files from the Finder.</li>
-
-            <li>Opens .duck bookmark files from the Finder.</li>
-          </ul>
-        </td>
-      </tr>
-
-      <tr>
-        <td class="box-solid" valign="top"><strong>ASCII and binary
-        FTP transfers</strong></td>
-
-        <td valign="top" colspan="2" class="box-dash">
-          <ul>
-            <li>Convert line endings on the fly in ASCII
-            transfers</li>
-
-            <li>Choose from Auto, Binary or ASCII</li>
-          </ul>
-        </td>
-      </tr>
-
-      <tr>
-        <td colspan="3">
-          <div align="right">
-            <a href=
-            "http://validator.w3.org/check/referer">Validate</a>,
-            $Date$
-          </div>
-
-          <div align="right">
-            <?php echo(echomenu(".."));?>
-          </div>
-        </td>
-      </tr>
-    </table>
+    <div class="feature">
+      <p><img src="./img/ftp.png" class="feature" alt="" />
+      <strong>Protocols</strong><br />
+      FTP (File Transfer Protocol), FTP/TLS (FTP secured over
+      SSL/TLS) and SFTP (SSH Secure File Transfer).</p>
+
+      <p><img src="./img/column.png" class="feature" alt="" />
+      <strong>Browser</strong><br />
+      Document based, list and outline view, caching, cut &amp;
+      paste, drag &amp; drop and arbitrary character encodings.</p>
+
+      <p><img src="./img/advanced.png" class="feature" alt="" />
+      <strong>International</strong><br />
+      Available in <em>English, Czech, Dutch, Finnish, French,
+      German, Italian, Japanese, Korean, Norwegian, Portuguese,
+      Slovak, Spanish, Chinese (Traditional and Simplified),
+      Russian, Swedish, Hungarian, Danish, Polish, Indonesian and
+      Catalan</em>.</p>
+
+      <p><img src="./img/textmate.png" class="feature" alt="" />
+      <strong>External editors</strong><br />
+      Seamless integration with external editors. <a href=
+      "http://www.codingmonkeys.de/subethaedit/">SubEthaEdit</a>,
+      <a href=
+      "http://www.barebones.com/products/bbedit/">BBEdit</a>,
+      <a href=
+      "http://www.barebones.com/products/textwrangler">TextWrangler</a>,
+      <a href="http://www.tex-edit.com/">Text-Edit Plus</a>,
+      <a href="http://macromates.com/">TextMate</a>, <a href=
+      "http://mimikaki.net/">mi</a>, <a href=
+      "http://smultron.sourceforge.net/">Smultron</a>, <a href=
+      "http://www.artman21.net/product/JeditX/">JeditX</a>,
+      <a href="http://macrabbit.com/cssedit/">CSSEdit</a>, <a href=
+      "http://www.aynimac.com/">CotEditor</a> and <a href=
+      "http://www.talacia.com/">Tag</a>.</p>
+
+      <p><img src="./img/gnu.png" class="feature" alt="" />
+      <strong>Open Source</strong><br />
+      Licensed under the GPL.</p>
+
+      <p><img src="./img/growl.png" class="feature" alt="" />
+      <strong>Growl</strong><br />
+      Support for Growl, the global notification system. See
+      <a href="http://growl.info/">growl.info</a>.</p>
+    </div>
+
+    <div class="feature">
+      <p><img src="./img/spotlight.png" class="feature" alt="" />
+      <strong>Spotlight</strong><br />
+      <a href=
+      "http://www.apple.com/macosx/features/spotlight/">Spotlight</a>
+      Importer for bookmark files.</p>
+
+      <p><img src="./img/document.png" class="feature" alt="" />
+      <strong>Bookmarking</strong><br />
+      Powerful bookmarking in a the browser drawer. Drag and drop
+      bookmarks to the Finder.app and drop files onto bookmarks to
+      upload.</p>
+
+      <p><img src="./img/rendezvous.png" class="feature" alt="" />
+      <strong>Bonjour</strong><br />
+      Auto discovery of services on the local network</p>
+
+      <p><img src="./img/finder.png" class="feature" alt="" />
+      <strong>Integration</strong><br />
+      Use Cyberduck as default system wide protocol handler for FTP
+      (Requires <a href=
+      "http://www.rubicode.com/Software/RCDefaultApp">RCDefaultApp</a>).
+      Open .inetloc files and .duck bookmark files from the
+      Finder.</p>
+
+      <p><img src="./img/advanced.png" class="feature" alt="" />
+      <strong>Resumable Transfers</strong><br />
+      Both download and upload.</p>
+
+      <p><img src="./img/advanced.png" class="feature" alt="" />
+      <strong>Recursive Transfers.</strong><br />
+      Both download and upload.</p>
+
+      <p><img src="./img/applescript.png" class="feature" alt="" />
+      <strong>AppleScript</strong><br />
+      Full AppleScript integration. See the <a href=
+      "http://cvs.cyberduck.ch/cyberduck-cocoa/AppleScript%20Samples/">
+      sample scripts</a> (included on the disk image).</p>
+    </div>
+
+    <div class="feature">
+      <p><img src="./img/keychain.png" class="feature" alt="" />
+      <strong>Keychain</strong><br />
+      All passwords are stored in the system <a href=
+      "http://www.apple.com/macosx/features/security/">Keychain</a>
+      as Internet passwords available also to third party
+      applications.</p>
+
+      <p><img src="./img/idisk.png" class="feature" alt="" />
+      <strong>iDisk</strong><br />
+      Synchronize bookmarks with <a href=
+      "http://www.apple.com/macosx/features/dotmacsync/">.Mac
+      iDisk</a>.</p>
+
+      <p><img src="./img/advanced.png" class="feature" alt="" />
+      <strong>Synchronization</strong><br />
+      Synchronize local with remote directories (and vice versa)
+      and get a preview of affected files before any action is
+      taken.</p>
+
+      <p><img src="./img/advanced.png" class="feature" alt="" />
+      <strong>Permissions</strong><br />
+      Modify permissions on multiple files and recursivly.</p>
+
+      <p><img src="./img/advanced.png" class="feature" alt="" />
+      <strong>ASCII</strong><br />
+      Convert line endings on the fly in ASCII transfers.</p>
+
+      <p><img src="./img/sftp.png" class="feature" alt="" />
+      <strong>SSH</strong><br />
+      Supports Public key authentication, Keyboard Interactive
+      (PAM) Authentication, Support for various encryption ciphers
+      (3DES, Blowfish, Twofish, AES, CAST) and Authentication
+      algorithms (MD5, SHA1).</p>
+    </div>
+  </div>
+
+  <div align="right">
+    <a href="http://validator.w3.org/check/referer">Validate</a>,
+    $Date$
+  </div>
+
+  <div align="right">
+    <?php echo(echomenu(".."));?>
   </div>
 </body>
 </html>
