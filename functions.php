87ba1bc03c9cb06da0d12abe70bb0a9cc24d281a
David Kocher <dkocher@iterate.ch>
2005-12-06 00:54:20 +0000

    *** empty log message ***

--- functions.php
+++ functions.php
@@ -53,12 +53,8 @@ function echodonate($dir) {
     function echotitle($dir) {
         return "<div id=\"title\"><h1 id=\"maintitle\">Cyberduck</h1><h3 id=\"subtitle\">FTP and SFTP Browser for Mac OS X. <b class=\"light\">Universal.</b></h3></div>
 <div id=\"donate\">
-<a href=\"$dir/donate\"><img id=\"amazon\" src=\"$dir/img/amazon.png\"
-	 alt=\"Amazon\"></img></a>
+	<a href=\"$dir/donate\"><img id=\"amazon\" src=\"$dir/img/amazon.png\" alt=\"Amazon\"></img></a>
 ".echodonate($dir)."
-<!--		<a href=\"$dir/donate\"><img id=\"paypal\" src=\"$dir/img/paypal.gif\"
-		 alt=\"Paypal\"></img></a>-->
-		
 		</div>";
     }
     
