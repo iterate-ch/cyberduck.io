f8de191d402caeca1eb971a116a9bdc4a9a21817
David Kocher <dkocher@iterate.ch>
2006-01-17 10:12:22 +0000

    *** empty log message ***

---
+++ screenshots.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+  include "functions.php";
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta name="generator" content=
+	"HTML Tidy for Mac OS X (vers 1st December 2004), see www.w3.org" />
+	<?php echo(echoheader(".")); ?>
+
+	<title>Cyberduck | FTP for Mac OS X. | Screenshots</title>
+</head>
+
+<body id="screenshots">
+	<div class="header">
+		<?php echo(echologo("."));?><?php echo(echotitle("."));?><?php echo(echosubmenu("."));?>
+	</div>
+
+	<div class="content">
+		<table cellpadding="5" cellspacing="5" width="100%">
+			<tr>
+				<td class="box-solid">
+					<div align="center">
+						<div class="feature">
+							<a href="./img/screenshots/info.png"><img width="200" border="0"
+							src="./img/screenshots/info.png" alt=
+							"Info Panel" /></a>
+							<p style="text-align:center;">Info Panel</p>
+						</div>
+
+						<div class="feature">
+							<a href="./img/screenshots/toolbar_customization.png">
+							<img border="0" width="200"
+							src="./img/screenshots/toolbar_customization.png" alt=
+							"Toolbar Customization" /></a>
+							<p style="text-align:center;">Toolbar Customization</p>
+						</div>
+
+						<div class="feature">
+							<a href="./img/screenshots/preferences.png">
+							<img border="0" width="200"
+							src="./img/screenshots/preferences.png" alt=
+							"Preferences" /></a>
+							<p style="text-align:center;">Preferences</p>
+						</div>
+					</div>
+				</td>
+			</tr>
+
+			<tr>
+				<td class="box-solid">
+					<div align="center">
+						<div class="feature">
+							<a href="./img/screenshots/bookmark.png"><img width="200" border="0"
+							src="./img/screenshots/bookmark.png" alt=
+							"Bookmark Panel" /></a>
+							<p style="text-align:center;">Bookmark Edit Panel</p>
+						</div>
+
+						<div class="feature">
+							<a href="./img/screenshots/transfers.png">
+							<img border="0" width="200"
+							src="./img/screenshots/transfers.png" alt=
+							"Transfers Window" /></a>
+						</div>
+
+						<div class="feature">
+							<a href="./img/screenshots/toolbar_customization.png">
+							<img border="0" width="200"
+							src="./img/screenshots/toolbar_customization.png" alt=
+							"Toolbar Customization" /></a>
+						</div>
+					</div>
+				</td>
+			</tr>
+			
+			<tr>
+				<td><?php echo(echogoogle());?></td>
+			</tr>
+
+			<tr>
+				<td>
+					<div class="footer">
+						<a href="http://validator.w3.org/check/referer">Validate</a>, $Date$<br />
+					</div>
+				</td>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
