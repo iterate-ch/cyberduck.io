760ee71c8c7051da9eace3708c1e219e9a939ca7
David Kocher <dkocher@iterate.ch>
2006-01-22 11:09:48 +0000

    *** empty log message ***

--- changelog.php
+++ changelog.php
@@ -32,7 +32,7 @@
 
       <tr>
         <td class="box-solid" width="150" valign="top">
-        <strong>2.5.4</strong> <em>Jan-19-2006</em><br />
+        <strong>2.5.4</strong> <em>Jan-20-2006</em><br />
         <a href="./Cyberduck-2.5.4.dmg">Download</a></td>
 
         <td colspan="2" class="box-solid">
