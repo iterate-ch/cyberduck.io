ad674f8af9095c25c433bd5a24ef65594b87c6db
David Kocher <dkocher@iterate.ch>
2011-02-10 09:17:27 +0000

    Switch to CDN

--- changelog/index.en.php
+++ changelog/index.en.php
@@ -2343,7 +2343,7 @@
 				</table>
 			</div>
 			<div id="footer">
-				<a class="swissmade" href="http://www.swissmadesoftware.org"><img src="http://media.cyberduck.ch/img/sms-logo-small-footer.png" alt="swiss made software" /></a> <a href=
+				<a class="swissmade" href="http://www.swissmadesoftware.org"><img src="http://cdn.cyberduck.ch/img/sms-logo-small-footer.png" alt="swiss made software" /></a> <a href=
 				"http://validator.w3.org/check/referer">Validate</a>, $Date$<br />
 			</div>
 		</div>
