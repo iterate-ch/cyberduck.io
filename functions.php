96c5041ab21aa03c8cb46437ee3187e04f90e70d
David Kocher <dkocher@iterate.ch>
2013-05-24 13:15:17 +0000

    Switch to SSL for analytics

--- functions.php
+++ functions.php
@@ -17,7 +17,7 @@
 
 				(function() {
 					var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-					ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+					ga.src = 'https://ssl.google-analytics.com/ga.js';
 					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 				})();
 			</script>
@@ -54,12 +54,6 @@
 			</span>";
 		}
 
-	function echoamazon() {
-		return "<script type='text/javascript'><!--
-		amazon_ad_tag = 'httpcybech-21'; amazon_ad_width = '728'; amazon_ad_height = '90'; amazon_ad_price = 'retail'; amazon_ad_border = 'hide';//--></script>
-		<script type='text/javascript' src='http://www.assoc-amazon.de/s/ads.js'></script>";
-	}
-	
 	function echologo($dir) {
 		return "
 			<div id='metanav'>
