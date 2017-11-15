8dbb465a72d9db5aea25a58891a5ffbc4a9b22dd
David Kocher <dkocher@iterate.ch>
2017-11-15 09:44:39 +0000

    /usr/local/bin/pelican

--- fabfile.py
+++ fabfile.py
@@ -15,14 +15,14 @@ def clean():
         local('mkdir {deploy_path}'.format(**env))
 
 def build():
-    local('pelican -s pelicanconf.py')
+    local('/usr/local/bin/pelican -s pelicanconf.py')
 
 def rebuild():
     clean()
     build()
 
 def regenerate():
-    local('pelican -r -s pelicanconf.py')
+    local('/usr/local/bin/pelican -r -s pelicanconf.py')
 
 def serve():
     os.chdir(env.deploy_path)
@@ -41,4 +41,4 @@ def reserve():
     serve()
 
 def preview():
-    local('pelican -s publishconf.py')
+    local('/usr/local/bin/pelican -s publishconf.py')
